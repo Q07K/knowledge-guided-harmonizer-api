7d1dea69c48afe33d0e6e69f28e94ce2ff52b93a
gyuhyeong <kgh0730@gmail.com>
2025-09-20 00:36:24 +0900

    Feat: 로깅 기능 추가 및 초기화 로거 호출

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from app.core.logging import initialize_loggers
+from app.utils.decorators.logging import log_execution
+
+
+@log_execution(log_args=True, log_result=True)
 def main():
     print("Hello from knowledge-guided-harmonizer-api!")
 
 
 if __name__ == "__main__":
+    # 로거 초기화
+    initialize_loggers()
     main()
