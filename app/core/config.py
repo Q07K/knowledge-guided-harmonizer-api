450c2231ee69b108b9ae2d97b03e12295a4dd968
gyuhyeong <kgh0730@gmail.com>
2025-09-23 19:46:25 +0900

    Feat: 설정 관리를 위한 `Settings` 클래스 추가
    pydantic-settings를 사용하여 시스템 전반의 설정을 관리하는 Settings 클래스 추가

---
+++ app/core/config.py
@@ -0,0 +1,31 @@
+from functools import cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # LLM 설정
+    model: str = "gemini-2.5-flash"
+    temperature: float | None = None
+    max_tokens: int | None = None
+
+    # Gemini API KEY
+    gemini_api_key: str
+
+    # Settings Config
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+
+@cache
+def get_settings() -> Settings:
+    """Settings 객체를 반환하는 함수
+
+    Returns
+    -------
+    Settings
+        설정 객체
+    """
+    return Settings()
