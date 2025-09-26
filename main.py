1e1816314ec403159920327e2980626f03a7e0de
gyuhyeong <kgh0730@gmail.com>
2025-09-27 08:58:05 +0900

    Feat(main): FastAPI 인스턴스 생성 및 CORS 미들웨어 추가
    - FastAPI 인스턴스를 생성하여 API 서버 기능을 구현
    - CORS 미들웨어를 추가하여 특정 출처에서의 요청을 허용
    - init_message 라우터를 등록하여 초기 메시지 처리 기능 통합

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from knowledge-guided-harmonizer-api!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import init_message
+
+# FastAPI 인스턴스 생성
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    middleware_class=CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "*",
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+# 라우터 등록
+app.include_router(router=init_message.router)
